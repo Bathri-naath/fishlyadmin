885597dc468f2c1abe70f7f60a9299f2190160fa
Bathri-naath <bathri768@gmail.com>
2024-12-02 19:46:50 +0530

    dashboard component rename

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ import { RootState } from "./store";
 import { login } from "./store";
 import Login from "./components/Login";
 // import { Dispatch } from "react";
-import Dashboard from "./components/Dashboard";
+import Dashboard from "./components/Users";
 import UserDetails from "./components/UserDetails";
 import UserOrdersPage from "./components/Orders";
 import Admin from "./components/Admin";
 
 const App: React.FC = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const { isLoggedIn } = useSelector((state: RootState) => state.auth);
 
   useEffect(() => {
@@ -28,14 +28,15 @@ const App: React.FC = () => {
       <Route
         path="/"
         element={
-          isLoggedIn ? <Navigate to="/admin/dashboard" /> : <Navigate to="/login" />
+          isLoggedIn ? (
+            <Navigate to="/admin/dashboard" />
+          ) : (
+            <Navigate to="/login" />
+          )
         }
       />
       <Route path="/login" element={<Login />} />
-      <Route
-        path="/admin/*"
-        element={<Admin />}
-      >
+      <Route path="/admin/*" element={<Admin />}>
         {/* Admin-specific pages */}
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="orders" element={<UserOrdersPage />} />
